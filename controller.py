efbf2ef9690881600bba84f3dfa43cf283d01322
Anton Kargapolov <igloooooook@gmail.com>
2023-07-20 16:14:50 +0500

    all phonebook is done

--- controller.py
+++ controller.py
@@ -1,37 +1,33 @@
 import model, view
 
+phonebook = []
+path = 'PhoneBook.txt'
 
 def start_menu():
     view.hi()
     while True:
-        print('1. Открыть файл с контактами \n'
-            '2. Записать файл с контактами \n'
-            '3. Добавить контакт \n'
-            '4. Изменить контакт \n'
-            '5. Удалить контакт \n'
-            '6. Поиск по контактам \n'
-            '0. Выход из программы \n')
+        view.menu()
         answer = int(input('Выберите действие: '))
         if answer == 1:
-            print('**********************************************')
+            view.separator()
             model.open_book()
             model.print_file()
-            print('**********************************************')
+            view.separator()
         elif answer == 2:
             model.save_in_contacts()
-            print('Файл успешно сохранен \n')
+            view.done()
         elif answer == 3:
             model.add_new_contact()
-            print('Контакт успешно добавлен \n')
+            view.done()
         elif answer == 4:
             model.change_in_contacts()
-            print('Контакт изменен')
+            view.done()
         elif answer == 5:
             model.delete_from_contacts()
-            print('Контакт удален')
+            view.done()
         elif answer == 6:
             model.search_in_contacts()
-            print('Контакт который вы искали')
+            view.done()
         else:
             print('Конец программы')
             break
